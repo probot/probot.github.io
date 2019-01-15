75a8275166682e38970f72371a0bbf3c6c77ee93
Jake Bolam <jake.bolam@gmail.com>
2019-01-15 15:54:36 +0800

    docs: add @all-contributors bot

---
+++ _apps/all-contributors.m
@@ -0,0 +1,18 @@
+---
+title: All Contributors
+description: A bot for automatically adding contributors per the all-contributors spec
+slug: all-contributors
+screenshots:
+- https://raw.githubusercontent.com/all-contributors/all-contributors-bot/master/docs/usage.png
+authors:
+- jakebolam
+repository: all-contributors/all-contributors-bot
+stars: 20
+updated: 2019-01-15 07:53:00 UTC
+host: https://gkioebvccg.execute-api.us-east-1.amazonaws.com/
+installations: 2
+organizations:
+- tophat
+---
+
+✨ Recognize all contributors, not just the ones who push code. Simple add a contributor with comments on issues or pull requests, e.g. `@all-contributors please add tbenning for design`
