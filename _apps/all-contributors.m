7a912a78fbe04c87cc1c000d32801d7d24d5d808
Jake Bolam <jake.bolam@gmail.com>
2019-01-21 13:57:59 -0500

    docs: update stats

--- _apps/all-contributors.m
+++ _apps/all-contributors.m
@@ -7,12 +7,16 @@ screenshots:
 authors:
 - jakebolam
 repository: all-contributors/all-contributors-bot
-stars: 20
-updated: 2019-01-15 07:53:00 UTC
-host: https://gkioebvccg.execute-api.us-east-1.amazonaws.com/
-installations: 2
+stars: 39
+updated: 2019-01-21 18:48:00 UTC
+host: https://gkioebvccg.execute-api.us-east-1.amazonaws.com/prod
+installations: 16
 organizations:
+- all-contributors
+- thechangelog
 - tophat
+- react-native-community
+- dowjones
 ---
 
 ✨ Recognize all contributors, not just the ones who push code. Simple add a contributor with comments on issues or pull requests, e.g. `@all-contributors please add tbenning for design`
